bdce68f3ab2de8da6e421a339e3fc5dd555aefc9
MarcelinoGNeto <marcelino.gneto@gmail.com>
2023-04-17 15:50:48 -0300

    Colisao e fisica

--- Scripts/ControlaJogador.cs
+++ Scripts/ControlaJogador.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ControlaJogador : MonoBehaviour
 {
     public float Velocidade = 10;
+    Vector3 direcao;
 
     // Update is called once per frame
     void Update()
@@ -13,9 +14,7 @@ public class ControlaJogador : MonoBehaviour
         float eixoX = Input.GetAxis("Horizontal");
         float eixoZ = Input.GetAxis("Vertical");
 
-        Vector3 direcao =  new Vector3(eixoX, 0, eixoZ);
-
-        transform.Translate(direcao * Velocidade * Time.deltaTime);
+        direcao =  new Vector3(eixoX, 0, eixoZ);
 
             if(direcao != Vector3.zero){
                 GetComponent<Animator>().SetBool("Movendo", true);
@@ -23,5 +22,10 @@ public class ControlaJogador : MonoBehaviour
                 GetComponent<Animator>().SetBool("Movendo", false);
             }
             
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + (direcao * Velocidade * Time.deltaTime));
     }
 }
