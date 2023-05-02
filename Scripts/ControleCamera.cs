e8cebd1111257662f0dd8efe0a4189beb9907fdc
MarcelinoGNeto <marcelino.gneto@gmail.com>
2023-05-02 17:27:00 -0300

    feat: finalizando jogo

--- Scripts/ControleCamera.cs
+++ Scripts/ControleCamera.cs
@@ -6,7 +6,7 @@ public class ControleCamera : MonoBehaviour
 {
 
     public GameObject Jogador;
-    Vector3 distCompensar;
+    private Vector3 distCompensar;
 
     // Start is called before the first frame update
     void Start()
